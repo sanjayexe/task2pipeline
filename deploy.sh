6cc8ad82118d6053c7c94e96482f363117a0b245
sanjay <117289265+sanjayexe@users.noreply.github.com>
2025-02-05 10:25:42 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u sanjay188 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
+    docker login -u sanjay188 -p virat@18vk
     docker tag test sanjay188/repo-1
     docker push sanjay188/repo-1
     
